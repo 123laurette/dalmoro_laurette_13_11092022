a6f68bb5e73c6916ffad6dceff387bb6164083c6
laurette <dalmorolaurette@orange.fr>
2022-09-20 13:39:32 +0200

    suppresion de code use effect

--- front_argentbanck/src/pages/SignIn.jsx
+++ front_argentbanck/src/pages/SignIn.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import {useNavigate, useParams} from "react-router-dom"
-import fetchData from "../services/fetchApi"
+//import {useNavigate, useParams} from "react-router-dom"
+//import fetchData from "../services/fetchApi"
 import "./../style/SignIn.css"
 import HeaderSignIn from '../components/header_SignIn/Header_SignIn';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
+
 
 
 function SignIn() {
+    return (
+        <div>
+            <HeaderSignIn/>
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-wrapper">
+                            <label >Username</label>
+                            <input type="email" id="email" />
+                        </div>
+                        <div className="input-wrapper">
+                            <label >Password</label>
+                            <input type="current-password" id="data.password" />
+                        </div>
+                        <div className="input-remember">
+                            <input type="checkbox" id="remember-me" /><label>Remember me</label>
+                        </div>
+                        <button className="sign-in-button" type='submit'>Sign In</button>
+
+                    </form>
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default SignIn;
+
+/*function SignIn() {
     const {id} = useParams()
     console.log (id)
     const navigate = useNavigate()
@@ -27,35 +59,4 @@ function SignIn() {
 
     if(!dataUser){
         return null
-    }
-    return (
-        <div>
-            <HeaderSignIn/>
-            <main className="main bg-dark">
-                <section className="sign-in-content">
-                    <i className="fa fa-user-circle sign-in-icon"></i>
-                    <h1>Sign In</h1>
-                    <form>
-                        <div className="input-wrapper">
-                            <label >Username</label>
-                            <input type="email" id="email" value={`${dataUser.data.email}`} />
-                        </div>
-                        <div className="input-wrapper">
-                            <label >Password</label>
-                            <input type="password" id="data.password" />
-                        </div>
-                        <div className="input-remember">
-                            <input type="checkbox" id="remember-me" /><label>Remember me</label>
-                        </div>
-                        <button className="sign-in-button" type='submit'>Sign In</button>
-
-                    </form>
-                </section>
-            </main>
-        </div>
-    );
-};
-
-export default SignIn;
-
-
+    }*/
